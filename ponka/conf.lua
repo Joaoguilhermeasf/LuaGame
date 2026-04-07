463f91bf11df719b8677e818edd07600a1541df8
Mobile User <mobile@localhost>
2026-04-06 21:34:20 -0300

    save local changes at 2026-04-06 21:34:20

--- ponka/conf.lua
+++ ponka/conf.lua
@@ -3,6 +3,8 @@
 function love.conf(t)
     t.window.width = 0  
     t.window.height = 0  
-    --t.window.fullscreen = true
-    --t.window.fullscreentype = "desktop" 
+    t.window.fullscreen = true
+    t.window.fullscreentype = "desktop"
+    t.window.orientation="landscapeft"
+
 end
