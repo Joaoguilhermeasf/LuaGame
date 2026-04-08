dba0cbc2ec34621e8ad7b75e2867612626999071
João Guilherme <joaoguilherme762@gmail.com>
2026-04-08 11:31:48 -0300

    Assets fix

--- levels/level1.lua
+++ levels/level1.lua
@@ -27,12 +27,10 @@ function level.load()
     player.jumps = 0
 
     -- Chão
-
-    local groundHeight = 100
-        
+    
     ground = {}
-    ground.body = love.physics.newBody(world, sw / 2, 550 + groundHeight / 2, "static")
-    ground.shape = love.physics.newRectangleShape(5000, groundHeight)
+    ground.body = love.physics.newBody(world, 0, 550, "static")
+    ground.shape = love.physics.newRectangleShape(sw, 900)
     ground.fixture = love.physics.newFixture(ground.body, ground.shape)
     ground.fixture:setUserData({allowJump = true})
 
