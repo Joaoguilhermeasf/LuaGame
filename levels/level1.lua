87b2b11a9154de96d3fa17d2b53e848d7598252c
João Guilherme <joaoguilherme762@gmail.com>
2026-04-08 21:30:01 -0300

    .

--- levels/level1.lua
+++ levels/level1.lua
@@ -157,7 +157,7 @@ function level.draw()
     local xB, yB = ground.body:getWorldPoints(ground.shape:getPoints())
     local scale = (sw * 0.25) / bush:getWidth()
     xB = xB * 0.9
-    yB = yB - yB * 0.8
+    yB = yB - (yB * 0.8)
     love.graphics.setColor(1, 1, 1)
     love.graphics.draw(bush, xB, yB, 0, scale, scale)
 
