91e692f46cca49e3d93c8126dc27b2bf9046eb6e
João Guilherme <joaoguilherme762@gmail.com>
2026-04-10 08:17:02 -0300

    conf

--- conf.lua
+++ conf.lua
@@ -1,8 +1,18 @@
--- FULLSCREEN
-
 function love.conf(t)
-    t.window.width = 0  
-    t.window.height = 0  
-    --t.window.fullscreen = true
-    --t.window.fullscreentype = "desktop" 
+    t.identity = "ponka"
+    t.version = "11.4"
+
+    t.window.title = "Ponka High Res"
+
+    t.window.width = 1280
+    t.window.height = 720
+    t.window.highdpi = true
+    t.window.usedpiscale = true
+    t.window.resizable = true
+    t.window.borderless = false
+    t.window.fullscreen = false
+
+    t.modules.physics = true
+    t.modules.touch = true
+    t.modules.graphics = true
 end
